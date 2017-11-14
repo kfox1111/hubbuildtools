dbf1dfc0f6d6156890b49e28a6860e3213c55297
Kevin Fox <kevin@efox.cc>
2017-11-14 11:08:33 -0800

    Fix lack of which on some platforms.

--- fingerprint.inner.sh
+++ fingerprint.inner.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-which pip 2>/dev/null > /dev/null \
+[ -x /usr/bin/pip ] \
     && echo pip \
     && (pip freeze --disable-pip-version-check | sort);
 
-which dpkg 2>/dev/null > /dev/null \
+[ -x /usr/bin/dpkg-query ] \
     && echo dpkg \
     && (dpkg-query -W -f='${binary:Package}-${Version}.${Architecture}\n' | sort -k 2);
 
-which rpm 2>/dev/null > /dev/null \
+[ -x /usr/bin/rpm ] \
     && echo rpm \
     && (rpm -qa | sort)
