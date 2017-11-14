6c8a3167d1a81a197f77dc40781b8026f6c1ebb7
Kevin Fox <kevin@efox.cc>
2017-11-14 11:14:50 -0800

    Shorten the fingerprint.

--- fingerprint.sh
+++ fingerprint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
-docker run -i --rm -v "$DIR/fingerprint.inner.sh":/fingerprint.sh --entrypoint /bin/sh "$1" /fingerprint.sh
+docker run -i --rm -v "$DIR/fingerprint.inner.sh":/fingerprint.sh --entrypoint /bin/sh "$1" /fingerprint.sh | bzip2 -c| base64 | tr '\n' '=' | sed 's/=//g'
