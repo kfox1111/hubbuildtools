84a8c5e04ce0d3a0433db5274e33cd66bf30dc03
Kevin Fox <kevin@efox.cc>
2017-11-14 10:20:37 -0800

    set entrypoint to sh

--- fingerprint.sh
+++ fingerprint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
-docker run -i --rm -v "$DIR/fingerprint.inner.sh":/fingerprint.sh --entrypoint /fingerprint.sh "$1"
+docker run -i --rm -v "$DIR/fingerprint.inner.sh":/fingerprint.sh --entrypoint /bin/sh "$1" -- -c /fingerprint.sh
