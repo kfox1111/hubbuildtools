4c72707689133d3b6cc612cad972483b2a982c81
Kevin Fox <kevin@efox.cc>
2017-11-14 14:39:48 -0800

    unescape

--- hubcurlfingerprint.sh
+++ hubcurlfingerprint.sh
@@ -8,6 +8,6 @@ echo Fetching fingerprint for image: $IMAGE
 
 alias jqc="docker run -i --rm devorbitus/ubuntu-bash-jq-curl jq"
 
-TOKEN=$(curl -s -L "https://auth.docker.io/token\?service\=registry.docker.io\&scope\=repository:$IMAGE:pull" | jqc -r .token)
+TOKEN=$(curl -s -L "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$IMAGE:pull" | jqc -r .token)
 
 curl -s -L -H "Authorization: Bearer $TOKEN" "https://registry-1.docker.io/v2/$IMAGE/manifests/$TAG" | jqc -r .history[].v1Compatibility | jqc '.container_config.Labels."com.github.kfox1111.fingerprint"'
