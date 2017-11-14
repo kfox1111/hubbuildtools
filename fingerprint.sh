438f81340d3dac9b05287c656e44fc6950be349b
Kevin Fox <kevin@efox.cc>
2017-11-14 10:06:44 -0800

    Remove extra quote

--- fingerprint.sh
+++ fingerprint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
-docker run -it --rm -v "$DIR/fingerprint.inner.sh":/fingerprint.sh" --entrypoint /fingerprint.sh
+docker run -it --rm -v "$DIR/fingerprint.inner.sh":/fingerprint.sh --entrypoint /fingerprint.sh
