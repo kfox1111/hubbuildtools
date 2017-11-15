f88719dbde84f015b1d837a517b61aa7fd359f67
Kevin Fox <kevin@efox.cc>
2017-11-15 11:40:50 -0800

    Fix issue with docker spewing bad info into streams due to downloading.

--- common.sh
+++ common.sh
@@ -1,4 +1,8 @@
+function setupjq () {
+  docker pull devorbitus/ubuntu-bash-jq-curl
+}
+
 function jq () {
-  docker run -i --rm devorbitus/ubuntu-bash-jq-curl jq "$@" 2>&1
+  docker run -i --rm devorbitus/ubuntu-bash-jq-curl jq "$@"
 }
 
