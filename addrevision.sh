c942e9dab4ed38988c5382db97d6ca684f8cbcfc
Kevin Fox <kevin@efox.cc>
2017-11-14 17:17:50 -0800

    Fix up revision linkage.

--- addrevision.sh
+++ addrevision.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
-revisionimage="$1"
-finalimage="$2"
+IMAGE="$1"
+TAG="$2"
+FINALIMAGE="$3"
+PREFIX="$4"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
-REVISION=$("$DIR/findrevision.sh" "$revisionimage")
-docker build --build-arg image="$revisionimage" --build-arg revision="$REVISION" -t "$finalimage" -f $DIR/Dockerfile.revision "$DIR"
+REVISION=$("$DIR/fetchnewrevision.sh" "$IMAGE" "$TAG" "$prefix")
+docker build --build-arg image="$revisionimage" --build-arg revision="$REVISION" -t "$FINALIMAGE" -f $DIR/Dockerfile.revision "$DIR"
