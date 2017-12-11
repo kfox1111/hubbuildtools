8844ffaadbd0b8ec4122266f9faace126d4492e2
Kevin Fox <kevin@efox.cc>
2017-12-11 13:21:04 -0800

    Switch to the container compatible way of using jq.

--- buildquine.sh
+++ buildquine.sh
@@ -26,16 +26,16 @@ EOF
 
 MANIFEST=$(cat registry/docker/registry/v2/repositories/$REGNAME/_manifests/tags/latest/current/link | sed 's/sha256://')
 MANIFEST2PARENT="registry/v2/blobs/sha256/$(echo $MANIFEST | cut -c1-2)/$MANIFEST/data"
-PARENTSHA=$(jq -r '.config.digest' registry/docker/$MANIFEST2PARENT | sed 's/^sha256://')
+PARENTSHA=$(cat registry/docker/$MANIFEST2PARENT | jq -r '.config.digest' | sed 's/^sha256://')
 PARENTPREFIX=$(echo $PARENTSHA | cut -c1-2)
 MANIFESTPARENT=registry/v2/blobs/sha256/$PARENTPREFIX/$PARENTSHA/data
 #DIRROOT="/var/lib/registry/docker"
 DIRROOT=""
 DIRPREFIX="$DIRROOT/registry/v2/repositories/$REGNAME"
 
-jq -c '.config.container_config.image="sha256:'$PARENTSHA'" | .container_config.Cmd=["/bin/sh", "-c", "#(nop) ADD multi:foo in / "] | .container_config.Image="sha256:'$PARENTSHA'" | .history += [{"created": "2017-11-29T15:29:13.963036127Z","created_by":"/bin/sh -c #(nop) ADD multi:foo in / "}] | .rootfs.diff_ids += ["sha256:@DATASHA@"]' registry/docker/$MANIFESTPARENT > rawdata/manifest1.json.tmpl
+cat registry/docker/$MANIFESTPARENT | jq -c '.config.container_config.image="sha256:'$PARENTSHA'" | .container_config.Cmd=["/bin/sh", "-c", "#(nop) ADD multi:foo in / "] | .container_config.Image="sha256:'$PARENTSHA'" | .history += [{"created": "2017-11-29T15:29:13.963036127Z","created_by":"/bin/sh -c #(nop) ADD multi:foo in / "}] | .rootfs.diff_ids += ["sha256:@DATASHA@"]' > rawdata/manifest1.json.tmpl
 
-jq '.layers+=[{"mediaType":"application/vnd.docker.image.rootfs.diff.tar.gzip","size":"@DIFFSIZE@","digest":"sha256:@DIFFSHA@"}] | .config.size="@MANIFESTSIZE@" | .config.digest = "sha256:@MANIFESTSUM@"' registry/docker/$MANIFEST2PARENT > rawdata/manifest2.json.tmpl
+cat registry/docker/$MANIFEST2PARENT | jq '.layers+=[{"mediaType":"application/vnd.docker.image.rootfs.diff.tar.gzip","size":"@DIFFSIZE@","digest":"sha256:@DIFFSHA@"}] | .config.size="@MANIFESTSIZE@" | .config.digest = "sha256:@MANIFESTSUM@"' > rawdata/manifest2.json.tmpl
 
 rm -f data | true
 tar -cvf data rawdata
