50ebd9f32786f032aca198c13d7beb3f6bb67310
Kevin Fox <kevin@efox.cc>
2017-12-12 08:01:32 -0800

    Add retry loop.

--- common.sh
+++ common.sh
@@ -13,11 +13,23 @@ function startregistry () {
 }
 
 function buildregistry () {
+  NAME=registry
   IMAGE=registry
   PORT=5000
   docker pull registry
   docker tag $IMAGE localhost:$PORT/$REGNAME
-  docker push localhost:$PORT/$REGNAME
+  end=$(date +%s)
+  end=$((end + 180)) #2 min
+  set +xe
+  while true; do
+    sleep 5
+    #The registry is not always emediately ready. Try for a bit to get it.
+    #FIXME what prefix to use? we need it to be unique in the registry....
+    docker push localhost:$PORT/$REGNAME
+    now=$(date +%s)
+    [ $now -gt $end ] && echo failed to start container && docker logs $NAME && exit -1
+  done
+  set -xe
 }
 
 function stopregistry() {
