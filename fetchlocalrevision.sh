835d735d4f502775eb2f117187aaf6846a08a361
Kevin Fox <Kevin.Fox@pnnl.gov>
2019-04-05 15:32:32 -0700

    Another fix for docker changing its schema

--- fetchlocalrevision.sh
+++ fetchlocalrevision.sh
@@ -5,4 +5,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 . "$DIR"/common.sh
 
 LOCALIMAGE="$1"
-docker inspect "$LOCALIMAGE" | jq -r '.[].ContainerConfig.Labels."com.github.kfox1111.revision"'
+REVISION=$(docker inspect "$LOCALIMAGE" | jq -r '.[].ContainerConfig.Labels."com.github.kfox1111.revision"')
+if [ "x$REVISION" == "xnull" ]; then
+	REVISION=$(docker inspect "$LOCALIMAGE" | jq -r '.[].Config.Labels."com.github.kfox1111.revision"')
+fi
+if [ "x$REVISION" == "xnull" ]; then
+	echo Failed to fetch revision >&2
+	exit -1
+fi
+echo $REVISION
